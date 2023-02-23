1f302261de16ab9971d345b28bf0c32f532261ef
shahreenbanu <85785868+shahreenbanu@users.noreply.github.com>
2023-02-22 17:26:54 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 function preload() {
 
 }
@@ -5,7 +7,18 @@ function setup() {
 canvas = createCanvas(600,600);
 camera = createCapture(VIDEO);
 camera.hide();
+poseNet = ml5.poseNet(camera,modelLoaded);
+poseNet.on('pose',gotResults);
 }
 function draw() {
 image(camera,0,0,600,600);
+}
+function modelLoaded() {
+console.log("pose net model is loaded");
+}
+function gotResults(results) {
+   console.log(results);
+   lips_x = results[0].pose.nose.x;
+lips_y = results[0].pose.nose.y;
+console.log(lips_x,lips_y);
 }
